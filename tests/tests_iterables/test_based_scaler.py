454753853afa9fcd7c1251b678dbaba74fd2a5c7
d.shchutskyi <d.shchutskyi@temabit.com>
2022-06-22 00:42:51 +0300

    fix last test for based_scaler

--- tests/tests_iterables/test_based_scaler.py
+++ tests/tests_iterables/test_based_scaler.py
@@ -18,5 +18,7 @@ def test_based_scaler() -> None:
     assert np.array_equal(based_scaler(np_2, np_1), np.array([1.5, 2]))
     assert np.array_equal(based_scaler(np_1, np_1), np_1)
     assert np.array_equal(based_scaler(np_2, np_3), np.array([0, 0]))
-    # bad numpy
-    # assert based_scaler(np_3, np_2).tolist() == [-np.inf, np.nan]
+    assert list(map(str, based_scaler(np_3, np_2).tolist())) == [
+        str(-np.inf),
+        str(np.nan),
+    ]
